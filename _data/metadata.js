52a9715c5ab74ea19df2df5a9c9552316f8de9f5
Paul Applegate <paul@theapplegates.com>
2023-07-08 19:05:08 -0400

    testing

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	"title": "Eleventy WebC Demo Blog",
-	"url": "https://example.com/",
+	"url": "https://11tywebctest.netlify.app",
 	"language": "en",
 	"description": "I am writing about my experiences as a naval navel-gazer.",
 	"author": {
 		"name": "Your Name Here",
 		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"url": "https://11tywebctest.netlify.app"
 	}
 };
