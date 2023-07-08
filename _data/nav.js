2a4089929ac52d7a5a4937549213cefdcf4a5ade
Paul Applegate <paul@theapplegates.com>
2023-07-08 19:01:51 -0400

    first commit

---
+++ _data/nav.js
@@ -0,0 +1,19 @@
+/**
+ * Site navigation data.
+ *
+ * @see _components/site-nav.webc
+ */
+
+module.exports = {
+	/** Site subdirectory.
+	 * @see eleventy.config.js
+	 */
+	// pathPrefix: '/11ty-webc-blog',
+
+	main: [
+    "./_pages/index.md",
+    "./_pages/about.md"
+		// "./_pages/test.1.md",
+		// "./_pages/test.2.md"
+  ]
+};
---
+++ _pages/index.md
@@ -0,0 +1,15 @@
+---
+layout: page
+# layout: home
+title: Home
+permalink: /
+---
+
+<message-box webc:nokeep></message-box>
+
+
+## Latest Posts
+
+<ol reversed class="postlist">
+  <collection-list tag="posts" reverse="true"></collection-list>
+</ol>
